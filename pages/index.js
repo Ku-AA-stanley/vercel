89307a25f7932c55d3c21aafd9b624ce5710fd3d
PengYang <plasma12563@gmail.com>
2024-10-15 10:24:38 +0800

    ngrok

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const HomePage = () => {
       <iframe
         width="100%"
         height="100%"
-        src="http://localhost:3000"
+        src="https://25b8-59-125-142-166.ngrok-free.app"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
